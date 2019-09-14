d41431ffa0f3277d043440d77d781061ea4677a6
kushnirolesya <kushnirolesya2000@gmail.com>
2019-09-14 17:53:35 +0300

    Account Sidebar Part

--- src/test/java/com/softserve/edu/opencart/pages/user/account/AccountSidebarPart.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/account/AccountSidebarPart.java
@@ -1,22 +1,58 @@
 package com.softserve.edu.opencart.pages.user.account;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.softserve.edu.opencart.pages.user.common.BreadCrumbPart;
+import org.openqa.selenium.WebElement;
 
-public class AccountSidebarPart extends BreadCrumbPart {
-	public AccountSidebarPart(WebDriver driver) {
-		super(driver);
-		initElements();
-	}
+public abstract class AccountSidebarPart extends BreadCrumbPart {
 
-	private void initElements() {
-		// init elements
-	}
+    protected WebElement myAccount;
+    protected WebElement login;
+    protected WebElement register;
+    protected WebElement forgottenPassword;
+    protected WebElement editAccount;
+    protected WebElement changePassword;
+    protected WebElement addressBook;
+    protected WebElement wishList;
+    protected WebElement orderHistory;
+    protected WebElement downloads;
+    protected WebElement recurringPayments;
+    protected WebElement rewardPoints;
+    protected WebElement returns;
+    protected WebElement transactions;
+    protected WebElement newsletter;
+    protected WebElement logOut;
 
-	// Page Object
 
-	// Functional
+    public AccountSidebarPart(WebDriver driver) {
+        super(driver);
+        initElements();
+    }
 
-	// Business Logic
+    private void initElements() {
+        login = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Login')]"));
+        register = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Register')]"));
+        forgottenPassword = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Forgotten Password')]"));
+        myAccount = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'My Account')]"));
+        editAccount = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Edit Account')]"));
+        changePassword = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Password')]"));
+        addressBook = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Address Book')]"));
+        wishList = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Wish List')]"));
+        orderHistory = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Order History')]"));
+        downloads = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Downloads')]"));
+        recurringPayments = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Recurring payments')]"));
+        rewardPoints = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Reward Points')]"));
+        returns = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Returns')]"));
+        transactions = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Transactions')]"));
+        newsletter = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Newsletter')]"));
+        logOut = driver.findElement(By.xpath("//div[contains(@class,'list-group')]/a[contains(text(), 'Logout')]"));
+    }
+
+    // Page Object
+
+    // Functional
+
+    // Business Logic
 }
