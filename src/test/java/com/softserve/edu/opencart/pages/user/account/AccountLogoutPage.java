b269d75635b1cc906de37bc7341ee14654476c25
Victoria Tishchenko <vikatishtshenko@gmail.com>
2019-09-14 15:42:33 +0300

    ChangePasswordPage

--- src/test/java/com/softserve/edu/opencart/pages/user/account/AccountLogoutPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/account/AccountLogoutPage.java
@@ -1,8 +1,12 @@
 package com.softserve.edu.opencart.pages.user.account;
 
+import com.softserve.edu.opencart.pages.user.HomePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AccountLogoutPage extends AccountSidebarGuestPart {
+	private WebElement continueButton;
 
 	public AccountLogoutPage(WebDriver driver) {
 		super(driver);
@@ -10,12 +14,21 @@ public class AccountLogoutPage extends AccountSidebarGuestPart {
 	}
 
 	private void initElements() {
-		// init elements
+		continueButton = driver.findElement(By.cssSelector("a.btn.btn-primary"));
 	}
 
 	// Page Object
-
+	public WebElement getContinueButton() {
+		return continueButton;
+	}
+	public void clickContinueButton() {
+		getContinueButton().click();
+	}
 	// Functional
 
 	// Business Logic
+	public HomePage logout(){
+		clickContinueButton();
+		return new HomePage(driver);
+	}
 }
