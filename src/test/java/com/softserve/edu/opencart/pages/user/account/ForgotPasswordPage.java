fcb38ff1c74d30f5fd86ac334c2c5c663c8e75bd
Victoria Tishchenko <vikatishtshenko@gmail.com>
2019-09-14 15:01:27 +0300

    ForgottenPasswordPage

--- src/test/java/com/softserve/edu/opencart/pages/user/account/ForgotPasswordPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/account/ForgotPasswordPage.java
@@ -1,21 +1,59 @@
 package com.softserve.edu.opencart.pages.user.account;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ForgotPasswordPage extends AccountSidebarGuestPart {
+    private WebElement emailField;
+    private WebElement continueButton;
 
-	public ForgotPasswordPage(WebDriver driver) {
-		super(driver);
-		initElements();
-	}
+    public ForgotPasswordPage(WebDriver driver) {
+        super(driver);
+        initElements();
+    }
 
-	private void initElements() {
-		// init elements
-	}
+    private void initElements() {
+        emailField = driver.findElement(By.id("input-email"));
+        continueButton = driver.findElement(By.cssSelector("input.btn.btn-primary"));
+    }
 
-	// Page Object
+    // Page Object
+    //EmailField
+    public WebElement getEmailField() {
+        return emailField;
+    }
 
-	// Functional
+    public void clearEmailField() {
+        getEmailField().clear();
+    }
 
-	// Business Logic
+    public void clickEmailField() {
+        getEmailField().click();
+    }
+
+    public void setEmailField(String text) {
+        getEmailField().sendKeys(text);
+    }
+    //ContinueButton
+    public WebElement getContinueButton() {
+        return continueButton;
+    }
+    public void clickContinueButton() {
+        getContinueButton().click();
+    }
+
+    // Functional
+    private void fillEmailField(String text) {
+        clickEmailField();
+        clearEmailField();
+        setEmailField(text);
+    }
+
+    // Business Logic
+    private LoginPage sendEmailField(String email) {
+        fillEmailField(email);
+        clickContinueButton();
+        return  new LoginPage(driver);
+    }
 }
