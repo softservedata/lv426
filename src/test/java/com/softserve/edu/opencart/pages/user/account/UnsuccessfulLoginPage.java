6b2b52ee4aab7c0ef68c7ae45bb9e18318cde312
kushnirolesya <kushnirolesya2000@gmail.com>
2019-09-14 15:56:22 +0300

    Unsuccessful Login Page

--- src/test/java/com/softserve/edu/opencart/pages/user/account/UnsuccessfulLoginPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/account/UnsuccessfulLoginPage.java
@@ -1,8 +1,12 @@
 package com.softserve.edu.opencart.pages.user.account;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class UnsuccessfulLoginPage extends LoginPage {
+
+	private WebElement unsuccessfulLogin;
 
 	public UnsuccessfulLoginPage(WebDriver driver) {
 		super(driver);
@@ -10,12 +14,20 @@ public class UnsuccessfulLoginPage extends LoginPage {
 	}
 
 	private void initElements() {
-		// init elements
+		unsuccessfulLogin = driver.findElement(By.cssSelector("div.alert.alert-danger"));
 	}
 
 	// Page Object
 
+	public WebElement getUnsuccessfulLogin() {
+		return unsuccessfulLogin;
+	}
+
 	// Functional
+
+	public String getUnsuccessfulLoginText() {
+		return getUnsuccessfulLogin().getText();
+	}
 
 	// Business Logic
 }
