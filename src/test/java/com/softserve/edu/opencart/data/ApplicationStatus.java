410bccb58cb118ea3c1e745b9c792daaa09d1507
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:32:39 +0300

    Update ApplicationStatus.java

--- src/test/java/com/softserve/edu/opencart/data/ApplicationStatus.java
+++ src/test/java/com/softserve/edu/opencart/data/ApplicationStatus.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.softserve.edu.opencart.data;
 
 public final class ApplicationStatus {
@@ -29,37 +28,3 @@ public final class ApplicationStatus {
         this.isLogged = isLogged;
     }
 }
-=======
-package com.softserve.edu.opencart.data;
-
-public final class ApplicationStatus {
-
-	private static volatile ApplicationStatus instance = null;
-	//
-	private boolean isLogged;
-	
-	private ApplicationStatus() {
-		isLogged = false;
-	}
-	
-	public static ApplicationStatus get() {
-        if (instance == null) {
-            synchronized (ApplicationStatus.class) {
-                if (instance == null) {
-                    instance = new ApplicationStatus();
-                }
-            }
-        }
-        return instance;
-    }
-
-	public boolean isLogged() {
-		return isLogged;
-	}
-
-	public void setLogged(boolean isLogged) {
-		this.isLogged = isLogged;
-	}
-	
-}
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
