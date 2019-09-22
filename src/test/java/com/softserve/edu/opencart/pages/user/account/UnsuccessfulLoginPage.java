0f4ffda3e90c380490c060edbd52b7717a355710
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:55:37 +0300

    Update UnsuccessfulLoginPage.java

--- src/test/java/com/softserve/edu/opencart/pages/user/account/UnsuccessfulLoginPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/account/UnsuccessfulLoginPage.java
@@ -1,9 +1,5 @@
 package com.softserve.edu.opencart.pages.user.account;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,11 +9,7 @@ public class UnsuccessfulLoginPage extends LoginPage {
 	public static final String EXPECTED_LOGIN_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
 	//
 	private WebElement alertWarning;
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
 	public UnsuccessfulLoginPage(WebDriver driver) {
 		super(driver);
 		initElements();
@@ -34,22 +26,12 @@ public class UnsuccessfulLoginPage extends LoginPage {
 		return alertWarning;
 	}
 
-<<<<<<< HEAD
-	// alertWarning
-	public WebElement getAlertWarning() {
-		return alertWarning;
-	}
-
 	public String getAlertWarningText() {
 		return getAlertWarning().getText();
 	}
 
-=======
-    public String getAlertWarningText() {
-        return getAlertWarning().getText();
-    }
     
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
+
 	// Functional
 
 	// Business Logic
