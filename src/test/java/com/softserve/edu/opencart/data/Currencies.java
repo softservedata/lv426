dc1891960591113284f6f064d0a8c6859a0b2687
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:30:15 +0300

    Update Currencies.java

--- src/test/java/com/softserve/edu/opencart/data/Currencies.java
+++ src/test/java/com/softserve/edu/opencart/data/Currencies.java
@@ -17,24 +17,4 @@ public enum Currencies {
         return name;
     }
 
-=======
-package com.softserve.edu.opencart.data;
-
-public enum Currencies {
-	EURO("Euro"),
-	POUND_STERLING("Pound Sterling"),
-	US_DOLLAR("US Dollar");
-	//
-	private String name;
-
-	private Currencies(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return name;
-	}
-
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
 }
