7cd6452cefb41bc78206a8956cf007de1af44962
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:34:31 +0300

    Update SearchFilter.java

--- src/test/java/com/softserve/edu/opencart/data/SearchFilter.java
+++ src/test/java/com/softserve/edu/opencart/data/SearchFilter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.softserve.edu.opencart.data;
 
 public class SearchFilter {
@@ -72,78 +71,4 @@ public class SearchFilter {
     }
 
 }
-=======
-package com.softserve.edu.opencart.data;
 
-public class SearchFilter {
-
-	private Product product;
-	private boolean isSimpleSearch;
-	private String categoryName;
-	private boolean useSubcategory;
-	private boolean useDescriptions;
-
-	public SearchFilter(Product product) {
-		this.product = product;
-		isSimpleSearch = true;
-		categoryName = new String();
-		useSubcategory = false;
-		useDescriptions = false;
-	}
-	
-	public SearchFilter(Product product, boolean isSimpleSearch,
-			String categoryName, boolean useSubcategory,
-			boolean useDescriptions) {
-		this.product = product;
-		this.isSimpleSearch = isSimpleSearch;
-		this.categoryName = categoryName;
-		this.useSubcategory = useSubcategory;
-		this.useDescriptions = useDescriptions;
-	}
-
-	// setters
-	
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	public void setSimpleSearch(boolean isSimpleSearch) {
-		this.isSimpleSearch = isSimpleSearch;
-	}
-
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
-
-	public void setUseSubcategory(boolean useSubcategory) {
-		this.useSubcategory = useSubcategory;
-	}
-
-	public void setUseDescriptions(boolean useDescriptions) {
-		this.useDescriptions = useDescriptions;
-	}
-
-	// getters
-	
-	public Product getProduct() {
-		return product;
-	}
-
-	public boolean isSimpleSearch() {
-		return isSimpleSearch;
-	}
-
-	public String getCategoryName() {
-		return categoryName;
-	}
-
-	public boolean isUseSubcategory() {
-		return useSubcategory;
-	}
-
-	public boolean isUseDescriptions() {
-		return useDescriptions;
-	}
-
-}
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
