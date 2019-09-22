459896fc9fa632a0fd07a285faacb1504932792d
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:48:15 +0300

    Update AccountLogoutPage.java

--- src/test/java/com/softserve/edu/opencart/pages/user/account/AccountLogoutPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/account/AccountLogoutPage.java
@@ -1,6 +1,5 @@
 package com.softserve.edu.opencart.pages.user.account;
 
-<<<<<<< HEAD
 import com.softserve.edu.opencart.data.ApplicationStatus;
 import com.softserve.edu.opencart.pages.user.HomePage;
 import org.openqa.selenium.By;
@@ -16,34 +15,12 @@ public class AccountLogoutPage extends AccountSidebarGuestPart {
         initElements();
         ApplicationStatus.get().setLogged(false);
     }
-=======
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import com.softserve.edu.opencart.data.ApplicationStatus;
-import com.softserve.edu.opencart.pages.user.HomePage;
-
-public class AccountLogoutPage extends AccountSidebarGuestPart {
-
-	private WebElement continueButton;
-	
-	public AccountLogoutPage(WebDriver driver) {
-		super(driver);
-		initElements();
-		ApplicationStatus.get().setLogged(false);
-	}
-
-	private void initElements() {
-		continueButton = driver.findElement(By.cssSelector("a.btn.btn-primary[href*='common/home']"));
-	}
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
 
     private void initElements() {
         continueButton = driver.findElement(By.cssSelector("a.btn.btn-primary[href*='common/home']"));
     }
 
-<<<<<<< HEAD
     // Page Object
 
     // continueButton
@@ -67,28 +44,4 @@ public class AccountLogoutPage extends AccountSidebarGuestPart {
         clickContinueButton();
         return new HomePage(driver);
     }
-=======
-	// continueButton
-	public WebElement getContinueButton() {
-		return continueButton;
-	}
-
-    public String getContinueButtonText() {
-        return getContinueButton().getText();
-    }
-
-	public void clickContinueButton() {
-		getContinueButton().click();
-	}
-
-	// Functional
-
-	// Business Logic
-
-	public HomePage gotoContinue(){
-		clickContinueButton();
-		return new HomePage(driver);
-	}
-
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
 }
