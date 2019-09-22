cc23a635c384d192005309086bb7da72da8f5894
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:33:54 +0300

    Update Product.java

--- src/test/java/com/softserve/edu/opencart/data/Product.java
+++ src/test/java/com/softserve/edu/opencart/data/Product.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.softserve.edu.opencart.data;
 
 import java.util.HashMap;
@@ -60,66 +59,4 @@ public class Product {
     }
 
 }
-=======
-package com.softserve.edu.opencart.data;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Product {
-
-	private String name;
-	private String description;
-	private String priceDollarExTax;
-	// TODO
-	//private Map<Currencies, Decimal> prices;
-	private Map<Currencies, String> prices;
-
-	public Product(String name, String description, String priceDollarExTax) {
-		this.name = name;
-		this.description = description;
-		this.priceDollarExTax = priceDollarExTax;
-		prices = new HashMap<Currencies, String>();
-	}
-
-	// setters
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public void setPriceDollarExTax(String priceDollarExTax) {
-		this.priceDollarExTax = priceDollarExTax;
-	}
-
-	public Product addPrice(Currencies currency, String price)
-    {
-        prices.put(currency, price);
-        return this;
-    }
-	
-	// getters
-	
-	public String getName() {
-		return name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public String getPriceDollarExTax() {
-		return priceDollarExTax;
-	}
-
-    public String getPrice(Currencies currency)
-    {
-        return prices.get(currency);
-    }
-
-}
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
