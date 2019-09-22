1014ab12d76fafd470b538f305e1f60d8fcf5d6f
adomanych34 <54119541+adomanych34@users.noreply.github.com>
2019-09-22 15:31:50 +0300

    Update ProductRepository.java

--- src/test/java/com/softserve/edu/opencart/data/ProductRepository.java
+++ src/test/java/com/softserve/edu/opencart/data/ProductRepository.java
@@ -2,7 +2,6 @@ package com.softserve.edu.opencart.data;
 
 public final class ProductRepository {
 
-<<<<<<< HEAD
     private ProductRepository() {
     }
 
@@ -22,24 +21,4 @@ public final class ProductRepository {
     }
 
 }
-=======
-	private ProductRepository() {
-	}
 
-	public static Product getDefault() {
-		return getMacBook();
-	}
-
-	public static Product getMacBook() {
-		return new Product("MacBook", "Intel Core 2 Duo processor Powered by an Intel Core 2 Duo processor at speeds up to 2.1..", "500")
-				.addPrice(Currencies.EURO, "472.33")
-				.addPrice(Currencies.POUND_STERLING, "368.73")
-				.addPrice(Currencies.US_DOLLAR, "602.00");
-	}
-
-	public static Product getInvalid() {
-		return new Product("Bok", "Intel", "100");
-	}
-
-}
->>>>>>> 40c679a4d7b258b0d93712d093a711e40eed356b
