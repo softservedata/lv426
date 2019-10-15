774932f5f329e00c0862306148cf577b866e9b22
adomanych34 <oshustarsen4@gmail.com>
2019-10-15 22:19:39 +0300

    add reset system method

--- src/test/java/com/softserve/edu/rest/services/BaseService.java
+++ src/test/java/com/softserve/edu/rest/services/BaseService.java
@@ -1,4 +1,13 @@
 package com.softserve.edu.rest.services;
 
+import com.softserve.edu.rest.entity.SimpleEntity;
+import com.softserve.edu.rest.resources.ResetResource;
+
 public class BaseService {
+
+    public GuestService reset() {
+        ResetResource resetResource = new ResetResource();
+        SimpleEntity simpleEntity = resetResource.httpGetAsEntity(null, null);
+        return new GuestService();
+    }
 }
