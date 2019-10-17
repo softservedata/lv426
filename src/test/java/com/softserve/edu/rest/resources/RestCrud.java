1c7824008c43109dc1ae81a474e2f70b5ac43863
RockerDeveloper <chapchaimax@gmail.com>
2019-10-17 09:50:45 +0300

    lock test

--- src/test/java/com/softserve/edu/rest/resources/RestCrud.java
+++ src/test/java/com/softserve/edu/rest/resources/RestCrud.java
@@ -13,17 +13,18 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public abstract class RestCrud {
-	private final String NOT_SUPPORT_MESSAGE = "Method %s not Support for %s Resource";
+    private final String NOT_SUPPORT_MESSAGE = "Method %s not Support for %s Resource";
     //
     private final String URL_PARAMETERS_SEPARATOR = "?";
     private final String NEXT_PARAMETERS_SEPARATOR = "&";
     private final String KEY_VALUE_SEPARATOR = "=";
+    private final String PATH_LEFT_BRACE = "{";
+    private final String PATH_RIGHT_BRACE = "}";
     //
     private RestUrl restUrl;
     private OkHttpClient client;
 
-    public RestCrud(RestUrl restUrl)
-    {
+    public RestCrud(RestUrl restUrl) {
         this.restUrl = restUrl;
         client = new OkHttpClient();
     }
@@ -48,19 +49,14 @@ public abstract class RestCrud {
 
     // Parameters - - - - - - - - - - - - - - - - - - - -
 
-    private String prepareUrlParameters(String urlTemplate, RestParameters urlParameters)
-    {
-        if (urlParameters != null)
-        {
+    private String prepareUrlParameters(String urlTemplate, RestParameters urlParameters) {
+        if (urlParameters != null) {
             boolean isFirstParameter = true;
             for (String currentKey : urlParameters.getAllParameters().keySet()) {
-                if (isFirstParameter)
-                {
+                if (isFirstParameter) {
                     urlTemplate = urlTemplate + URL_PARAMETERS_SEPARATOR;
                     isFirstParameter = false;
-                }
-                else
-                {
+                } else {
                     urlTemplate = urlTemplate + NEXT_PARAMETERS_SEPARATOR;
                 }
                 urlTemplate = urlTemplate + currentKey + KEY_VALUE_SEPARATOR + urlParameters.getParameter(currentKey);
@@ -70,23 +66,25 @@ public abstract class RestCrud {
     }
 
     private String preparePathVariables(String urlTemplate, RestParameters pathVariables) {
-    	String url = urlTemplate;
-    	String searchVariable;
+        String url = urlTemplate;
+        String searchVariable;
         if (pathVariables != null) {
             for (String currentKey : pathVariables.getAllParameters().keySet()) {
-            	// TODO Create Const
-                searchVariable = "{" + currentKey + "}";
+                searchVariable = PATH_LEFT_BRACE + currentKey + PATH_RIGHT_BRACE;
                 if (url.contains(searchVariable)) {
-                	// TODO Move to RegexUtils
-                    url = url.replaceFirst(Pattern.quote(searchVariable), pathVariables.getParameter(currentKey));
+                    url = pathPreparing(url, searchVariable, pathVariables, currentKey);
                 }
             }
         }
         return url;
     }
 
+    private String pathPreparing(String url, String searchVariable, RestParameters pathVariables, String currentKey) {
+        return url.replaceFirst(Pattern.quote(searchVariable), pathVariables.getParameter(currentKey));
+    }
+
     private RequestBody prepareRequestBody(RestParameters bodyParameters) {
-    	RequestBody requestBody = null;
+        RequestBody requestBody = null;
         if (bodyParameters != null) {
             FormBody.Builder formBodyBuilder = new FormBody.Builder();
             for (String currentKey : bodyParameters.getAllParameters().keySet()) {
@@ -100,7 +98,7 @@ public abstract class RestCrud {
     // Request - - - - - - - - - - - - - - - - - - - -
 
     private Request.Builder prepareRequestBuilder(String requestUrl, RestParameters pathVariables,
-            RestParameters urlParameters) {
+                                                  RestParameters urlParameters) {
         if ((requestUrl == null) || (requestUrl.isEmpty())) {
             // TODO Develop Custom Exception
             //log.error("prepareRequestBuilder() Error ");
@@ -111,7 +109,7 @@ public abstract class RestCrud {
         url = prepareUrlParameters(url, urlParameters);
         return new Request.Builder().url(url);
     }
-    
+
     private Response executeRequest(Request request) {
         Response result = null;
         try {
@@ -123,7 +121,7 @@ public abstract class RestCrud {
         }
         return result;
     }
-    
+
     private String responseBodyAsText(Response response) {
         String responseText = null;
         try {
@@ -136,7 +134,7 @@ public abstract class RestCrud {
         }
         return responseText;
     }
-    
+
     // Http Get - - - - - - - - - - - - - - - - - - - -
 
     protected Response httpGetAsResponse(RestParameters pathVariables, RestParameters urlParameters) {
@@ -144,45 +142,45 @@ public abstract class RestCrud {
     }
 
     protected String httpGetAsText(RestParameters pathVariables, RestParameters urlParameters) {
-    	return responseBodyAsText(httpGetAsResponse(pathVariables, urlParameters));
+        return responseBodyAsText(httpGetAsResponse(pathVariables, urlParameters));
     }
 
     // Http Post - - - - - - - - - - - - - - - - - - - -
 
     protected Response httpPostAsResponse(RestParameters pathVariables, RestParameters urlParameters,
-            RestParameters bodyParameters) {
+                                          RestParameters bodyParameters) {
         return executeRequest(prepareRequestBuilder(restUrl.readPostUrl(), pathVariables, urlParameters)
                 .post(prepareRequestBody(bodyParameters)).build());
     }
 
     protected String httpPostAsText(RestParameters pathVariables, RestParameters urlParameters,
-            RestParameters bodyParameters) {
+                                    RestParameters bodyParameters) {
         return responseBodyAsText(httpPostAsResponse(pathVariables, urlParameters, bodyParameters));
     }
 
     // Http Put - - - - - - - - - - - - - - - - - - - -
 
     protected Response httpPutAsResponse(RestParameters pathVariables, RestParameters urlParameters,
-            RestParameters bodyParameters) {
+                                         RestParameters bodyParameters) {
         return executeRequest(prepareRequestBuilder(restUrl.readPutUrl(), pathVariables, urlParameters)
                 .put(prepareRequestBody(bodyParameters)).build());
     }
 
     protected String httpPutAsText(RestParameters pathVariables, RestParameters urlParameters,
-            RestParameters bodyParameters) {
+                                   RestParameters bodyParameters) {
         return responseBodyAsText(httpPutAsResponse(pathVariables, urlParameters, bodyParameters));
     }
 
     // Http Delete - - - - - - - - - - - - - - - - - - - -
 
     protected Response httpDeleteAsResponse(RestParameters pathVariables, RestParameters urlParameters,
-            RestParameters bodyParameters) {
+                                            RestParameters bodyParameters) {
         return executeRequest(prepareRequestBuilder(restUrl.readDeleteUrl(), pathVariables, urlParameters)
                 .delete(prepareRequestBody(bodyParameters)).build());
     }
 
     protected String httpDeleteAsText(RestParameters pathVariables, RestParameters urlParameters,
-            RestParameters bodyParameters) {
+                                      RestParameters bodyParameters) {
         return responseBodyAsText(httpDeleteAsResponse(pathVariables, urlParameters, bodyParameters));
     }
 
