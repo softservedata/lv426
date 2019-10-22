cb97f9740685762dce3817eab3dc4849ef9dbe2c
Marta Fina <fmarta172@gmail.com>
2019-10-22 12:31:30 +0300

    testtttt

--- src/test/java/com/softserve/edu/rest/test/ItemsTest.java
+++ src/test/java/com/softserve/edu/rest/test/ItemsTest.java
@@ -3,11 +3,19 @@ package com.softserve.edu.rest.test;
 import com.softserve.edu.rest.data.*;
 import com.softserve.edu.rest.services.GuestService;
 import com.softserve.edu.rest.services.UserService;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+
+
 public class ItemsTest {
+
+    public static final Logger logger = (Logger) LoggerFactory.getLogger(ItemsTest.class);
 
     @DataProvider
     public Object[][] itemget() {
@@ -19,23 +27,25 @@ public class ItemsTest {
 
     @Test(dataProvider = "itemget")
     public void getAllItemsTest(User user, Item firstItem, User admin, Item secondItem) {
+        logger.info("getAllItemsTest() START");
         UserService userService = new GuestService()
                 .successfulUserLogin(user)
                 .addItem(firstItem)
                 .addItem(secondItem);
+        logger.trace("log in user " + user.getName() + " and add item " + firstItem.getItem());
 
 
 
         Assert.assertTrue(userService.getAllItems().contains(secondItem.getItem()));
         Assert.assertTrue(userService.getAllItems().contains(firstItem.getItem()));
         userService.logoutUser();
-
+        logger.info("getAllItemsTest() FINISH");
     }
 
 
     @Test(dataProvider = "itemget")
     public void getAllItemsIndexesTest(User user, Item firstItem, User admin, Item secondItem) {
-
+        logger.info("getAllItemsIndexesTest() START");
         UserService adminService = new GuestService()
                 .successfulUserLogin(admin)
                 //.addItem(firstItem)
@@ -50,12 +60,14 @@ public class ItemsTest {
         Assert.assertTrue(userService.getAllItemsIndexes().contains(firstItem.getIndex()));
 
         userService.logoutUser();
+        logger.info("getAllItemsIndexesTest() FINISH");
     }
 
 
 
     @Test(dataProvider = "itemget")
     public void getUserItemsTest(User user, Item firstItem, User admin, Item secondItem) {
+        logger.info("getUserItemsTest() START");
         UserService userService = new GuestService()
                 .successfulUserLogin(user)
                 .addItem(firstItem)
@@ -68,11 +80,13 @@ public class ItemsTest {
         Assert.assertTrue(adminService.getUserItems(user).contains(firstItem.getItem()));
         Assert.assertTrue(adminService.getUserItems(user).contains(secondItem.getItem()));
         adminService.logoutUser();
+        logger.info("getUserItemsTest() FINISH");
 
     }
 
     @Test(dataProvider = "itemget")
     public void getUserItemTest(User user, Item firstItem, User admin, Item secondItem) {
+        logger.info("getUserItemTest() START");
         UserService userService = new GuestService()
                 .successfulUserLogin(user)
                 .addItem(firstItem)
@@ -84,6 +98,7 @@ public class ItemsTest {
 
         Assert.assertTrue(adminService.getUserItem(firstItem, user).contains(firstItem.getItem()));
         adminService.logoutUser();
+        logger.info("getUserItemsTest() FINISH");
     }
 
 
